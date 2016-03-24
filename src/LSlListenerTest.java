cf09f7bbae10a218d47eb2dac2b4dd4f61103472
gxi505 <gxi505@cs.bham.ac.uk>
2016-03-24 15:57:36 +0000

    Presentation of route execution

---
+++ src/LSlListenerTest.java
@@ -0,0 +1,32 @@
+
+import lejos.nxt.LightSensor;
+
+/**Class that simulates the real listener
+ * @author Gabriel Iuriciuc
+ *
+ */
+public class LSlListenerTest extends Thread {
+
+	private LineFollowTest robotUnit;
+	private LightSensor right;
+	private LightSensor left;
+	public static boolean stop = true;
+
+	public LSlListenerTest(LineFollowTest robotUnit, LightSensor right, LightSensor left) {
+		this.robotUnit = robotUnit;
+		this.right = right;
+		this.left = left;
+	}
+
+	@Override
+	public void run() {
+		int rightlightValue;
+		int leftlightValue;
+		while(stop) {
+			rightlightValue = right.getLightValue();
+			leftlightValue = left.getLightValue();
+			robotUnit.leftSensorValue = leftlightValue;
+			robotUnit.rightSensorValue = rightlightValue;
+		}
+	}
+}
---
+++ src/Route.java
@@ -0,0 +1,22 @@
+
+/**
+ * Class that tests the behaviour of the robot with simple actions
+ * @author Gabriel Iuriciuc
+ *
+ */
+public class Route{
+	
+	public static void main(String[] args) {
+		LineFollowTest lineFollower = new LineFollowTest();
+		// Constructed an imaginary route to test
+			lineFollower.moveAction(4);
+			lineFollower.turnAction(90);
+			lineFollower.moveAction(5);
+			lineFollower.turnAction(-90);
+			lineFollower.moveAction(1);
+			lineFollower.turnAction(-90);
+			lineFollower.moveAction(2);
+			lineFollower.turnAction(-90);
+	}
+}
+
