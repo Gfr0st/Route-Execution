deb7f920e7f23b8ac69144704b915e326e990b27
Gabriel Iuriciuc <gabriel.iuriciuc97@gmail.com>
2016-12-23 21:31:59 +0200

    Improved the documentation

--- src/LineFollower.java
+++ src/LineFollower.java
@@ -7,22 +7,21 @@ import lejos.robotics.navigation.DifferentialPilot;
 import lejos.util.Delay;
 
 /**
- * Class performs tests if the robot actually executes the given actions.
+ * Class used for moving the robot on the map.
  * 
  * @author Gabriel Iuriciuc
  *
  */
-public class LineFollowTest {
+public class LineFollower {
 
 	private DifferentialPilot pilot;
 	public int leftSensorValue;
 	public int rightSensorValue;
-	//changes travel speed
 	private final Float travelSpeed = 0.18f;
 
 	private int delay=50;
 	
-	public LineFollowTest(){
+	public LineFollower(){
 		pilot = new DifferentialPilot(0.056, 0.12, Motor.B, Motor.C);
 		delay = 50;
 		pilot.setTravelSpeed(travelSpeed);
@@ -38,16 +37,16 @@ public class LineFollowTest {
 		left.setLow(low);
 		right.setLow(low);
 		
-		LSlListenerTest ls1Listener = new LSlListenerTest(this , right ,left);
+		LSlListener ls1Listener = new LSlListener(this , right ,left); // setting up the listener for the 2 sensors
 		ls1Listener.start();
 	}
 	
-	public void moveAction(int distance){
+	public void moveAction(int distance){ // method for moving the robot 
 		while (distance > 0) {
-//			System.out.println("left"+ leftSensorValue);
-//			System.out.println("right" +rightSensorValue);
+			System.out.println("left"+ leftSensorValue); // debugging
+			System.out.println("right" +rightSensorValue); // debugging
 			if(leftSensorValue < 1500 && rightSensorValue < 1500){
-//				System.out.println("Junction");
+				System.out.println("Junction"); // debugging part for testing that the robot correctly identifies junctions on the map
 				distance--;
 				pilot.stop();
 			}else{
@@ -58,9 +57,7 @@ public class LineFollowTest {
 		}
 	}
 	
-	public void turnAction(double angle){
-		//distance to travel before turning
-//		System.out.println("turning");
+	public void turnAction(double angle){ // method for turning the robot
 		pilot.travel(0.03);
 		pilot.stop();
 		pilot.rotate(angle);
